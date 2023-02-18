9afa3f2e35fe882f6a7966c109ae0f3e1ced2d0b
syedabir455 <syedabir455@gmail.com>
2023-02-18 11:56:41 +0600

    rectangle part added

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-document.getElementById('btn-triangle').addEventListener('click',function(){
-    
-    // get input field data using id 
-    const triangle = document.getElementById('triangle').innerText;
-    const base = document.getElementById('triangle-base').value;
-    const height = document.getElementById('triangle-height').value;
-    
-    const triangleArea = 0.5 * parseFloat(base) * parseFloat(height);
+function displayData(triangle,triangleArea){
     const container = document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML =`
@@ -15,4 +8,39 @@ document.getElementById('btn-triangle').addEventListener('click',function(){
         
     `
     container.appendChild(tr);
+}
+
+document.getElementById('btn-triangle').addEventListener('click',function(){
+    
+    // get input field data using id 
+    const triangle = document.getElementById('triangle').innerText;
+    const base = document.getElementById('triangle-base').value;
+    const height = document.getElementById('triangle-height').value;
+    
+    const triangleArea = 0.5 * parseFloat(base) * parseFloat(height);
+displayData(triangle,triangleArea)
+    
 })
+
+document.getElementById('btn-rectangle').addEventListener('click',function(){
+   const rectangle = document.getElementById('rectangle').innerText;
+   const width = document.getElementById('rectangle-width').value;
+   const length = document.getElementById('rectangle-length').value;
+   const rectangleArea = parseFloat(width) * parseFloat(length);
+
+   displayData(rectangle,rectangleArea);
+   
+})
+
+// document.getElementById('btn-Parallelogram').addEventListener('click',function(){
+//     const parallelogram = document.getElementById('parallelogram').innerText;
+//     const base = document.getElementById('parallelogram-base').value;
+//     const height = document.getElementById('parallelogram-height').value;
+//     const parallelogramArea = parseFloat(base) * parseFloat(height);
+ 
+//     displayData(parallelogram,parallelogramArea);
+    
+//  })
+ 
+
+
